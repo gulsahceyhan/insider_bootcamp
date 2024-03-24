9d2224de8e54de6df00a3292620aff2f71c23521
Gülşah Ceyhan <cey.gulsah@hotmail.com>
2024-03-24 20:39:05 +0300

    second commit

---
+++ pipeline_bootcamp.groovy
@@ -0,0 +1,56 @@
+pipeline {
+    agent any
+    environment {
+        DEVELOPER BRANCH 'develop' 
+        BRANCH = "${BRANCH_NAME}"
+        ANSIBLE FORCE_COLOR= 'true'
+}
+
+stages {
+    stage('Get Branch Name') {
+        steps {
+            script {
+                branch = env.BRANCH
+            }
+        }
+    }
+
+
+    stage('Set Pending Status') {
+        steps {
+            script {
+                sh 'echo "Pending"'
+            }
+        }
+    } 
+
+    stage("Automation Precess") {
+        steps {
+            script {
+                dir('C:/Users/New/OneDrive/Masaüstü/bootcamp/test'){
+                    echo 'Automation Process running...'
+                    sh 'pip install -r requirements.txt'
+                    sh 'pwd'
+
+                    sh command
+                    sleep(time:30, unit 'SECONDS')
+                }
+            }
+        }
+    } 
+}
+
+post{
+    succes{
+        script{
+            sh 'echo "Automation process completed succesfully"'
+        }
+    }
+    failre{
+        script{
+            sh 'echo "Automation process failed"'
+        }
+    }
+}
+
+}
